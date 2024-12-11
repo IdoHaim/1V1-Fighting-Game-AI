696753f7764429daa235f970491fe86809bb6471
Ido Haim <137721224+IdoHaim@users.noreply.github.com>
2024-12-11 18:00:46 +0200

    Update AI_trainer.js

--- MyGame_1/scripts/AI_trainer.js
+++ MyGame_1/scripts/AI_trainer.js
@@ -18,46 +18,51 @@ export class AI_trainer{
         this.actions = Object.values(Commands);
         this.yourPlayer.AIupdateDelegate.addFunction(() => this.update());
     }
-    update(){ 
+   update(){ 
         if(this.game.Ai.isDataSaved){ // restard the game when its over and the data was saved
             this.game.goToMainMenu();
         }
         
         // handle decision making
-        let action;
+        let action = null;
 
+        // check possible threats
         let closestThreat = this.game.width;
-        this.enemyPlayer.attacks.forEach(attack => {
+        this.enemyPlayer.attacks.forEach(attack => { 
             let distance = Math.abs(attack.x + attack.width/2 - this.yourPlayer.x + this.yourPlayer.width/2);
             if(distance<closestThreat) closestThreat = distance;
         });
  
         if(closestThreat <= this.yourPlayer.width + this.yourPlayer.offsetX){            
-            action = this.chooseRandomAction(Commands.SHIELD,null,0.7);
+            action = this.chooseRandomAction(Commands.SHIELD,null,0.7); // making some of the shielding attempts fail on purpose
         }
         else if(Math.abs(this.yourPlayer.x - this.enemyPlayer.x) > this.yourPlayer.punch.width){
-            if(this.yourPlayer.x - this.enemyPlayer.x < 0){
-                action = this.chooseRandomAction(Commands.RUNRIGHT,Commands.SHOOT,0.99);
-            }
-            else{
-                action = this.chooseRandomAction(Commands.RUNLEFT,Commands.SHOOT,0.99);
+            if(this.chooseRandomAction(true,false,0.8)){ // make the trainer play slower
+
+                // getting closer to the enemy or shooting him
+                if(this.yourPlayer.x - this.enemyPlayer.x < 0){
+                    action = this.chooseRandomAction(Commands.RUNRIGHT,Commands.SHOOT,0.99);
+                }
+                else{
+                    action = this.chooseRandomAction(Commands.RUNLEFT,Commands.SHOOT,0.99);
+                }
             }
         }
         else{
+            // changing direction towards the enemy
             if(this.yourPlayer.direction === -1 && this.yourPlayer.x - this.enemyPlayer.x < 0){
                action = Commands.RUNRIGHT;
             }
             else if(this.yourPlayer.direction === 1 && this.yourPlayer.x - this.enemyPlayer.x > 0){
                 action = Commands.RUNRIGHT;
             }
+            // punch when the direction is towards the enemy
             else{
                 action = this.chooseRandomAction(Commands.PUNCH,null,0.7);
             }
         }
 
-        if(action != null){
-            this.input.AICommand(action);
-        }
+        this.input.AICommand(action);
     }
 
     chooseRandomAction(action1,action2,odds) { 
